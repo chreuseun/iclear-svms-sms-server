28739d6b0f7e6418dfaa64ffbb6eae26b8257e53
Eunille <eunille.villegas@growsari.com>
2020-11-08 17:59:40 +0800

    added config.template.js file

---
+++ config.template.js
@@ -0,0 +1,21 @@
+const SMS_PROVIDER_ENDPOINT = `https://api.semaphore.co/api/v4`
+const SMS_PROVIDER_API_KEY = ``
+const PORT = `7070`;
+const IP_ADDRESS = ``;
+const MYSQL_POOL_CREDENTIALS = {
+  connectionLimit : 10,
+  host            : '',
+  user            : '',
+  password        : '',
+  database        : '',
+  multipleStatements: true
+}
+
+const SMS_TEMPLATE = `` 
+
+module.exports.SMS_TEMPLATE = SMS_TEMPLATE
+module.exports.SMS_PROVIDER_API_KEY = SMS_PROVIDER_API_KEY;
+module.exports.SMS_PROVIDER_ENDPOINT = SMS_PROVIDER_ENDPOINT;
+module.exports.MYSQL_POOL_CREDENTIALS = MYSQL_POOL_CREDENTIALS;
+module.exports.PORT = PORT;
+module.exports.IP_ADDRESS = IP_ADDRESS;
