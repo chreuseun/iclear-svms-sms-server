78cf58c5644f3a18fae17e3c7ded0aa9f4af2fda
eunille <eunille.villegas@growsari.com>
2021-03-13 14:21:13 +0800

    added fix on config templte on getLocalIpAddress

--- src/lib/getLocalIpAddress/getLocalIpAddress.js
+++ src/lib/getLocalIpAddress/getLocalIpAddress.js
@@ -1,10 +1,38 @@
 'use strict';
 
-const { networkInterfaces } = require('os');
+const { networkInterfaces, platform } = require('os');
 const nets = networkInterfaces();
 
-const wifi = nets['Wi-Fi']
-const parseIpV4 = wifi.find(_wifi => _wifi.family === 'IPv4') || {}
-const IpV4 = parseIpV4.address || ''
+const windowsOps =  () => {
+    const wifi = nets['Wi-Fi']
+    const parseIpV4 = wifi.find(_wifi => _wifi.family === 'IPv4');
+
+    const {address =''} = parseIpV4 || {}
+    return  address || ''
+}
+
+const macOps =  () => {
+    const wifi = nets['en0']
+    const parseIpV4 = wifi.find(_wifi => _wifi.family === 'IPv4');
+    const {address =''} = parseIpV4 || {}
+     return  address || ''
+}
+
+
+const OPERATING_SYS =  {
+    darwin: macOps,
+    win32: windowsOps,
+    win64: windowsOps,
+    linux: macOps
+}
+
+const empt = () => {
+    return '###no-ip'
+}
+
+const osNetwork = OPERATING_SYS[platform()] ||  empt;
+const IP_V4 = osNetwork();
+
+console.log('READING DATA TO: ',IP_V4)
 
 module.exports = IpV4;
